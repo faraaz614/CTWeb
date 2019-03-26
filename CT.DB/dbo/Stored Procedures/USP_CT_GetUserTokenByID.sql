a697e3b87902a00b08f844f3c7a39faca0633fff
faraaz <faraaz614@gmail.com>
2019-03-27 00:24:36 +0530

    db update

---
+++ CT.DB/dbo/Stored Procedures/USP_CT_GetUserTokenByID.sql
@@ -0,0 +1,25 @@
+﻿--declare @statusid int
+--declare @msg varchar(100)
+--exec [USP_CT_GetUserTokenByID] 1,@statusid out, @msg out
+--select @statusid,@msg
+CREATE PROCEDURE [dbo].[USP_CT_GetUserTokenByID]
+(
+@UserID bigint,
+@Status int out,
+@Message nvarchar(500) out
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		SET  @Status = 1;
+		DECLARE @DealerID bigint;
+		Select @DealerID = DealerID from CT_TRAN_VehicleBID where ID = @UserID;
+		Select Token from CT_TRAN_UserToken where UserID = @DealerID
+	END TRY
+	BEGIN CATCH     
+		SET  @Status = 0;   
+		SET  @Message = 'Generic Error: '+ ERROR_MESSAGE() ;
+		EXECUTE USP_CT_GetErrorInfo;  
+	END CATCH
+END
---
+++ CT.DB/dbo/Tables/CT_TRAN_UserToken.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[CT_TRAN_UserToken] (
+    [ID]         INT           IDENTITY (1, 1) NOT NULL,
+    [Token]      VARCHAR (MAX) NULL,
+    [UserID]     INT           NULL,
+    [CreatedOn]  DATETIME      CONSTRAINT [DF_CT_TRAN_UserToken_CreatedOn] DEFAULT (getdate()) NULL,
+    [ModifiedOn] DATETIME      CONSTRAINT [DF_CT_TRAN_UserToken_UpdatedOn] DEFAULT (getdate()) NULL,
+    CONSTRAINT [PK_CT_TRAN_UserToken] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
